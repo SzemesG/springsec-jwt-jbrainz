2cf5e9a6f8e68ad5e9222700f50ecfd4c4f678b4
GaborSz <g.szemes@rockethome.de>
2019-12-12 10:45:06 +0100

    Initial commit

---
+++ src/main/java/de/rockethome/SpringSecJWTJBrainz/SpringSecJwtJBrainzApplication.java
@@ -0,0 +1,13 @@
+package de.rockethome.SpringSecJWTJBrainz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecJwtJBrainzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecJwtJBrainzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/rockethome/SpringSecJWTJBrainz/SpringSecJwtJBrainzApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rockethome.SpringSecJWTJBrainz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecJwtJBrainzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
